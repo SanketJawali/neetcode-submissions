0e3caaa15aff9a8db80d633a8e0daf4358bbb12e
Sanket Jawali <135835658+SanketJawali@users.noreply.github.com>
2026-08-25 09:57:01 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if len(nums) == 0: return 0
+
+        # Using 2 pointers, for bubbling up the val to the end
+        a, b = 0, 0
+
+        def countk():
+            k = len(nums)
+            for n in nums:
+                if n == val: k -= 1
+            return k
+
+        # Move a forward with each loop
+        # move b to find the next element which is not == val
+        while b < len(nums):
+            if nums[a] == val:
+                while nums[b] == val:
+                    if b == len(nums) - 1:
+                        print(nums, a, b)
+                        # b - a contains the value of k
+                        return countk()
+                    b += 1
+                nums[a] = nums[b]
+                nums[b] = val   # Required to count k
+            a += 1
+            if a > b:
+                b = a
+        print(nums, a, b)
+        return countk()
