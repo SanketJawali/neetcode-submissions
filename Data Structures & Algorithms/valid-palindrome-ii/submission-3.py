83bb440c6dab8fb787061f98e9bcc7fcd5b96881
Sanket Jawali <135835658+SanketJawali@users.noreply.github.com>
2026-09-02 01:30:21 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def palindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                # Skip one character and check for valid palindrome for rest of s
+                return palindrome(l + 1, r) or palindrome(l, r - 1)
+            l += 1
+            r -= 1
+
+        return True
