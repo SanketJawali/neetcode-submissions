ec67382b1db1778f0671f89d7cdfbebb8392b2da
Sanket Jawali <135835658+SanketJawali@users.noreply.github.com>
2026-09-24 13:27:32 +0530

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        
+        for i, c in enumerate(s):
+            if c != ']':
+                stack.append(c)
+                continue
+            
+            # Find encoded string within []
+            encoded = ""
+            while stack and stack[-1] != '[':
+                encoded = stack.pop() + encoded
+            stack.pop() # Pop '['
+
+            # Find multiplier k
+            k = ""
+            while stack and stack[-1].isdigit():
+                k = stack.pop() + k
+            for _ in range(int(k)):
+                stack.append(encoded)
+        
+        return "".join(stack)
